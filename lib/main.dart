2e7b069c0268893bb9d508e894e251e741114cd9
Ravindu Thakshila Jayasooriya <62090384+RavinduJP@users.noreply.github.com>
2024-10-31 14:22:46 +0530

    create local notification service class

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:local_notification/firebase_options.dart';
+
+
+Future<void> backgroundHandler(RemoteMessage message) async {
+  print("${message.data}");
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
   runApp(const MyApp());
 }
 
